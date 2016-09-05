a5d7e5e288092f3891a38e1e0ddeb5600d9a1ca4
Echsecutor <sebastian@schmittner.pw>
2016-09-05 21:09:27 +0200

    MWE fastcgi backend

--- fastcgi-test.go
+++ fastcgi-test.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"net"
+	"net/http"
+	"net/http/fcgi"
+	"strconv"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	var port int
+	var folder string
+	flag.IntVar(&port, "port", 2020, "port to listen on for fastcgi connections")
+	flag.StringVar(&folder, "folder", "C:\\", "folder to browse (just to return something)")
+	flag.Parse()
+
+	log.Println("Running fastcgi-test on localhost:", port, " serving a browser in", folder)
+
+	l, err := net.Listen("tcp", "localhost:"+strconv.Itoa(port))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	h := http.FileServer(http.Dir(folder))
+	log.Fatal(fcgi.Serve(l, h))
 }
